1810f2a62570a41a1120681075b78934c41de6b0
Owonikoko Ayomide <owonikokoayomide@gmail.com>
2025-03-05 00:45:06 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import "./app.css"
+import Navbar from './Components/Navbar/Navbar'
+import Home from './Components/Home/Home'
+import Mains from './Components/Mains/Mains'
+import Footer from './Components/Footer/Footer'
+
+function App() {
+  return (
+    <div>
+     <Navbar />
+     {/* <Home />
+     <Mains />
+     <Footer /> */}
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/Mains/Mains.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./mains.css"
+
+function Mains() {
+  return (
+    <div>Mains</div>
+  )
+}
+
+export default Mains
